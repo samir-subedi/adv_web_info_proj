723ae1ee9c92703ae662ad655e2fb67bf86d26d6
samir-subedi <samirsubedi145@gmail.com>
2024-09-15 21:09:04 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,79 @@
 <?php
-	include ("include/header_nav.php");
+    include "header.php";
+    include "connection.php";
+
+$sql = "SELECT * FROM product";
+$result = $conn -> query ($sql);
+
+
+if(isset($_POST['update_btn'])){
+  $update_id = $_POST['update_id'];
+  $name = $_POST['update_name'];
+  $des = $_POST['update_des'];
+  $unit = $_POST['update_unit'];
+  $unitprice = $_POST['update_unitprice'];
+  
+  $update_query = mysqli_query($conn, "UPDATE `product` SET unitprice = '$unitprice' , name='$name' , des='$des' ,unit='$unit'  WHERE id = '$update_id'");
+  if($update_query){
+     header('location:index.php');
+  };
+};
+
+if(isset($_GET['remove'])){
+  $remove_id = $_GET['remove'];
+  mysqli_query($conn, "DELETE FROM `product` WHERE id = '$remove_id'");
+  header('location:index.php');
+};
+
+
 ?>
 
-<main id = "index_main">
-	<h2 id = "h2_main">Welcome Page</h2>
-	<div id = "main_img_container">
-		<img id = "welcome_main" src="images/welcome/welcome.jpg" width="100%" 
-		/>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div class="container">
+    <h5>Stock Status</h5>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <!--<th scope="col">#</th>-->
+      <th scope="col">Product Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Unit</th>
+      <th scope="col">Unit Price</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+   
+      <?php
+          if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+              ?>
+             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+               <tr>
+                <input type="hidden" name="update_id"  value="<?php echo $row['id'];?>">
+                <td><input type="text" name="update_name"  value="<?php echo $row['name'];?>"></td>
+                <td><input type="text" name="update_des"  value="<?php echo $row['des'];?>"></td>
+                <td><input type="number" name="update_unit"  value="<?php echo $row['unit'];?>"></td>
+                <td><input type="number" name="update_unitprice"  value="<?php echo $row['unitprice'];?>"></td>
+                <td><button type="submit" class="btn btn-primary" name="update_btn">update</button></td>
+                <td><a  class="btn btn-primary" href="index.php?remove=<?php echo $row['id']; ?>">delete</a></td>
+                </tr>
+                </form>
+                <?php }
+        } else {
+            echo "0 results";
+        }
+        ?>
+      
 
-		<div id="welcome_message">
-			<h3>Tailor your wear to the best</h3>
-			<p>A great suit is the ultimate fashion statement for men, but it most certainly needs to be styled correctly. Nothing says disordered like a baggy suit, one that is ill-fitting or worse, a suit that features extra long trousers.</p>
-			<p>It is important to find a tailor who not only understands the ins and outs of making a <a href="#">great suit</a> but one that also appreciates your personal preferences when it comes to tailoring. </p>
-			<input type="button" value="We have done the hard work" id ="welcome-message-btn" >
-		</div>
-	</div>
-
-	<img id = "welcome_small" src="images/welcome/welcome_small1.jpg" width="25%"
-	/><img id = "welcome_small" src="images/welcome/welcome_small2.jpg" width="25%"
-	/><img id = "welcome_small" src="images/welcome/welcome_small3.jpg" width="25%"
-	/><img id = "welcome_small" src="images/welcome/welcome_small4.jpg" width="25%" />
-
-</main>
-
-<?php
-	include ("include/footer.php");
-?>
+    
+  </tbody>
+</table>
+</div>
+</body>
+</html>
